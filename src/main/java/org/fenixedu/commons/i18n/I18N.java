172cb38f5053f3355d6ad05aadcf19b46482e963
Pedro Santos <pedro.san7os@gmail.com>
2013-12-18 10:53:09 +0000

    Added license

--- src/main/java/org/fenixedu/commons/i18n/I18N.java
+++ src/main/java/org/fenixedu/commons/i18n/I18N.java
@@ -1,22 +1,20 @@
-/*
- * I18N.java
- *
+/**
  * Copyright (c) 2013, Instituto Superior Técnico. All rights reserved.
  *
  * This file is part of fenixedu-commons.
  *
  * fenixedu-commons is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * fenixedu-commons is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with fenixedu-commons.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with fenixedu-commons. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.fenixedu.commons.i18n;
 
--- src/main/java/org/fenixedu/commons/json/JsonAdapter.java
+++ src/main/java/org/fenixedu/commons/json/JsonAdapter.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (c) 2013, Instituto Superior Técnico. All rights reserved.
+ *
+ * This file is part of fenixedu-commons.
+ *
+ * fenixedu-commons is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * fenixedu-commons is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with fenixedu-commons. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.fenixedu.commons.json;
 
 public interface JsonAdapter<T> extends JsonCreator<T>, JsonUpdater<T>, JsonViewer<T> {
