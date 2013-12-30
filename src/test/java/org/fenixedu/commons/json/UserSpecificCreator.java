e84d1339f4e7c867998de18d09d0f7594de602e4
Pedro Santos <pedro.san7os@gmail.com>
2013-12-30 15:20:36 +0000

    Removed dependency from commons-codec

--- src/test/java/org/fenixedu/commons/json/UserSpecificCreator.java
+++ src/test/java/org/fenixedu/commons/json/UserSpecificCreator.java
@@ -18,8 +18,7 @@
  */
 package org.fenixedu.commons.json;
 
-import org.apache.commons.codec.binary.Base64;
-
+import com.google.common.io.BaseEncoding;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
@@ -29,7 +28,7 @@ public class UserSpecificCreator implements JsonCreator<User> {
     public User create(JsonElement jsonElement, JsonBuilder ctx) {
         JsonObject jsonObject = jsonElement.getAsJsonObject();
         final String name = jsonObject.get("name").getAsString();
-        final String password = Base64.encodeBase64String(jsonObject.get("password").getAsString().getBytes());
+        final String password = BaseEncoding.base64().encode(jsonObject.get("password").getAsString().getBytes());
         final String number = jsonObject.get("number").getAsString();
         return new User(name, password, number);
     }
