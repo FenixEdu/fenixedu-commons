764bbb0e5814f1be101d88c5f23def81fded202f
Luis Cruz <luis.cruz@tecnico.pt>
2017-09-28 16:18:28 +0100

    Add utility methods to help stream to json transformations

--- src/main/java/org/fenixedu/commons/stream/StreamUtils.java
+++ src/main/java/org/fenixedu/commons/stream/StreamUtils.java
@@ -2,6 +2,7 @@ package org.fenixedu.commons.stream;
 
 import java.util.Iterator;
 import java.util.Spliterators;
+import java.util.function.BiConsumer;
 import java.util.stream.Collector;
 import java.util.stream.Collector.Characteristics;
 import java.util.stream.Stream;
@@ -9,6 +10,7 @@ import java.util.stream.StreamSupport;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 /**
  * Utility methods to provide a bridge between Java 8 Streams and pre-Java 8 libraries.
@@ -17,6 +19,62 @@ import com.google.gson.JsonElement;
  *
  */
 public class StreamUtils {
+
+    /**
+     * Returns a {@link JsonArray} that results from applying the provided filler to each origin object.
+
+     * @param filler the filler to apply to the origin object to fill the JsonObject
+     * @param origin the original objects to be transformed
+     * 
+     * @return
+     *         A {@link JsonArray} that has been filled in by the filler function applied to each origin object.
+     */
+    public static <T> JsonArray toJsonArray(final BiConsumer<JsonObject, T> filler, final Iterable<T> origins) {
+        return toJsonArray(filler, origins.iterator());
+    }
+
+    /**
+     * Returns a {@link JsonArray} that results from applying the provided filler to each origin object.
+
+     * @param filler the filler to apply to the origin object to fill the JsonObject
+     * @param origin the original objects to be transformed
+     * 
+     * @return
+     *         A {@link JsonArray} that has been filled in by the filler function applied to each origin object.
+     */
+    public static <T> JsonArray toJsonArray(final BiConsumer<JsonObject, T> filler, final Stream<T> origins) {
+        return toJsonArray(filler, origins.iterator());
+    }
+
+    /**
+     * Returns a {@link JsonArray} that results from applying the provided filler to each origin object.
+
+     * @param filler the filler to apply to the origin object to fill the JsonObject
+     * @param origin the original objects to be transformed
+     * 
+     * @return
+     *         A {@link JsonArray} that has been filled in by the filler function applied to each origin object.
+     */
+    public static <T> JsonArray toJsonArray(final BiConsumer<JsonObject, T> filler, final Iterator<T> origins) {
+        final JsonArray result = new JsonArray();
+        origins.forEachRemaining(s -> result.add(toJson(filler, s)));
+        return result;
+    }
+
+    /**
+     * Returns a {@link JsonObject} that results from applying the provided filler to the origin object.
+
+     * @param filler the filler to apply to the origin object to fill the JsonObject
+     * @param origin the original object to be transformed
+     * 
+     * @return
+     *         A {@link JsonObject} that has been filled in by the filler function applied to the origin.
+     */
+    public static <T> JsonObject toJson(final BiConsumer<JsonObject, T> filler, final T origin) {
+         final JsonObject result = new JsonObject();
+         filler.accept(result, origin);
+         return result;
+     }
 
     /**
      * Returns a {@link Collector} that accumulates all the given {@link JsonElement}s into a new {@link JsonArray}.
@@ -48,4 +106,5 @@ public class StreamUtils {
         Iterator<T> iterator = (Iterator<T>) array.iterator();
         return StreamSupport.stream(Spliterators.spliterator(iterator, array.size(), 0), false);
     }
+
 }
